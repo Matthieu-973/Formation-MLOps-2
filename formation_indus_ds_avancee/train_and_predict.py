d2676758db231157015240e23164bdfc89fac6b8
matthieu-973 <mascoli@eiffage.com>
2026-05-04 12:46:55 +0000

    fix: vulture

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import joblib
 import pandas as pd
@@ -19,17 +18,6 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
     joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
-
-
-def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
-    features = pd.read_parquet(features_path)
-    features = predict(features, model_path)
-    time_str = time.strftime('%Y%m%d-%H%M%S')
-    features['predictions_time'] = time_str
-    features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, time_str + '.csv'),
-                                                         index=False)
-    features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, 'latest.csv'), index=False)
-
 
 def predict(features: pd.DataFrame, model_path: str) -> pd.DataFrame:
     model = joblib.load(model_path)
